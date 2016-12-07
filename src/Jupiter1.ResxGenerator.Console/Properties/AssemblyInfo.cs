8e6d12ac646c674f49bca7b49709a9d1148edfe6
Dmytry Ermakov <dmytro.yermakov.1985@gmail.com>
2016-12-07 11:38:25 +0200

    * Initial commit.

---
+++ src/Jupiter1.ResxGenerator.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Jupiter1.ResxGenerator.Console")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b69be481-3a2e-4dce-92ae-2208b934daf2")]
---
+++ src/Jupiter1.ResxGenerator.Console/ResourceData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jupiter1.ResxGenerator.Console
+{
+    internal class ResourceData
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public List<string> Arguments { get; set; }
+
+        public bool UsingNamedArgs { get; set; }
+
+        public string FormatArguments
+        {
+            get { return string.Join(", ", Arguments.Select(a => "\"" + a + "\"")); }
+        }
+
+        public string ArgumentNames
+        {
+            get { return string.Join(", ", Arguments.Select(GetArgName)); }
+        }
+
+        public string Parameters
+        {
+            get { return string.Join(", ", Arguments.Select(a => "object " + GetArgName(a))); }
+        }
+
+        public string GetArgName(string name)
+        {
+            return UsingNamedArgs ? name : 'p' + name;
+        }
+    }
+}
